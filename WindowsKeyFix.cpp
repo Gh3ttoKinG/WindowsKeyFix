1e635df541f980e001ce293d305d4824fca969c5
Gh3ttoK!nG <Gh3ttoKinG@users.noreply.github.com>
2021-05-14 17:59:57 +0200

    Extended by Windows Keys
    Extended by Windows Keys to disable functions like WinKey + F = Feedback Hub

--- WindowsKeyFix.cpp
+++ WindowsKeyFix.cpp
@@ -4,19 +4,33 @@
 #include <chrono>
 #include <iostream>
 
+#define UBound(n)	sizeof(n) / sizeof(n[0]);
+
 int main(int argc, wchar_t* argv[])
 {
-	//Build Array Of Keys To Unregister
+
+	//Build Array Of Hyper Keys To Unregister
 	//These map to W, T, Y, O, P, D, L, X, N, and Space, respectively.
-	UINT offendingKeys[10] = { 0x57, 0x54, 0x59, 0x4F, 0x50, 0x44, 0x4C, 0x58, 0x4E, 0x20 };
+	UINT offendingHyperKeys[10] = { 0x57, 0x54, 0x59, 0x4F, 0x50, 0x44, 0x4C, 0x58, 0x4E, 0x20 };
 
 	//Kill Explorer
 	system("taskkill /IM explorer.exe /F");
 
 	//Register hotkey
 	for (int i = 0; i < 10; i++) {
-		RegisterHotKey(NULL, i, 0x1 + 0x2 + 0x4 + 0x8 | MOD_NOREPEAT, offendingKeys[i]);
+		RegisterHotKey(NULL, i, 0x1 + 0x2 + 0x4 + 0x8 | MOD_NOREPEAT, offendingHyperKeys[i]);
 	}
+
+	//Build Array Of Windows Keys To Unregister
+	//These map to F, respectively.
+	int iCnt = UBound(offendingHyperKeys);
+
+	UINT offendingKeysWin[1] = { 0x46 };
+	for (int i = 0; i < 1; i++) {
+		RegisterHotKey(NULL, i + iCnt, 0x8 | MOD_NOREPEAT, offendingKeysWin[i]);
+	}
+	
+	iCnt = iCnt + UBound(offendingKeysWin);
 
 	//Restart Explorer
 	system("start C:/Windows/explorer.exe");
@@ -27,7 +41,7 @@ int main(int argc, wchar_t* argv[])
 	std::this_thread::sleep_for(std::chrono::milliseconds(4000));
 	 
 	//deregister hotkeys by ID
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < iCnt; i++) {
 		UnregisterHotKey(NULL, i);
 	}
 
